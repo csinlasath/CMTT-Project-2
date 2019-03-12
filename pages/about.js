e894abc48fa562fb6bd413afb0b1f5614d0c800f
Christopher Sinlasath <csinlasath@live.com>
2019-03-12 17:40:37 -0500

    Initial Commit

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Header from '../components/Header';
+import Layout from '../components/MyLayout.js';
+
+export default () => (
+    <Layout>
+        <p>This is the about page</p>
+    </Layout>
+);
---
+++ pages/index.js
@@ -0,0 +1,93 @@
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+const getPosts = () => {
+    return [
+        { id: 'page-1', title: 'Page 1' },
+        { id: 'page-2', title: 'Page 2' },
+        { id: 'page-3', title: 'Page 3' },
+    ];
+};
+
+const PostLink = ({ post }) => (
+    <li>
+        <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
+            <a>{post.title}</a>
+        </Link>
+        <style jsx>{`
+            li {
+                list-style: none;
+                margin: 5px;
+            }
+
+            a {
+                text-decoration: none;
+                color: blue;
+                font-family: "Arial";
+            }
+
+            a:hover {
+                opacity: 0.6;
+            }
+        `}</style>
+    </li>
+);
+
+// const Index = (props) => (
+//     <Layout>
+//         <h1>Batman TV Shows</h1>
+//         <ul>
+//             {props.shows.map(({ show }) => (
+//                 <li key={show.id}>
+//                     <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+//                         <a>{show.name}</a>
+//                     </Link>
+//                 </li>
+//             ))}
+//         </ul>
+//     </Layout>
+// );
+
+// Index.getInitialProps = async function () {
+//     const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+//     const data = await res.json();
+//     console.log(`Show data fetched.  Count: ${data.length}`);
+//     return {
+//         shows: data
+//     };
+// };
+
+export default () => (
+    <Layout>
+        <h1>Pet Tracker</h1>
+        <ul>
+            {getPosts().map((post) => (
+                <PostLink key={post.id} post={post} />
+            ))}
+        </ul>
+        <style jsx> {`
+                    h1, a {
+                        font-family: "Arial";
+                    }
+
+                    ul {
+                        padding: 0;
+                    }
+
+                    li {
+                        list-style: none;
+                        margin: 5px 0;
+                    }
+
+                    a {
+                        text-decoration: none;
+                        color: blue;
+                    }
+
+                    a:hover {
+                        opacity: 0.6;
+                    }
+                `}</style>
+    </Layout>
+);
