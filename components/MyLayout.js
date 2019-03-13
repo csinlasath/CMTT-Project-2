6ea177a4802a42337aa0e7b49192c755e4625875
Christopher Sinlasath <csinlasath@live.com>
2019-03-12 19:41:50 -0500

    Pretty-fied the initial Commit

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -1,16 +1,18 @@
-import Header from '../components/Header';
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+import Header from "../components/Header";
 
 const layoutStyle = {
-    margin: 20,
-    padding: 20,
-    border: '1px solid #DDD'
+  margin: 20,
+  padding: 20,
+  border: "1px solid #DDD"
 };
 
-const Layout = (props) => (
-    <div style={layoutStyle}>
-        <Header />
-        {props.children}
-    </div>
+const Layout = props => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+  </div>
 );
 
 export default Layout;
