d5553902215b822f1059b87bcbb1fde545f7ea8f
Christopher Sinlasath <csinlasath@live.com>
2019-03-13 17:00:24 -0500

    Delete petProfile.js

--- models/petProfile.js
+++
@@ -1,8 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-    const petProfiles = sequelize.define("petProfiles", {
-      petName: DataTypes.STRING,
-      breed: DataTypes.STRING,
-    });
-    return petProfiles;
-  };
-  
