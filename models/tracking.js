28c6b809e4308ced612fe6fa07a8947c5e009ccd
tiffanykeller <bakertiffany133@yahoo.com>
2019-03-16 13:36:07 -0500

    updates models

---
+++ models/tracking.js
@@ -0,0 +1,186 @@
+module.exports = (sequelize, DataTypes) => {
+  const tracking = sequelize.define("tracking", {
+    foodMeal: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 45]
+      }
+    },
+
+    foodMeasure: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1, 45]
+      }
+    },
+
+    foodNotes: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    waterMeasure: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1, 45]
+      }
+    },
+
+    waterMeal: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    waterNotes: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+
+    urine: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: {
+        len: [1, 45]
+      },
+
+      urineMeasure: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        validate: {
+          len: [1, 45]
+        },
+
+        urineColor: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          validate: {
+            len: [1, 45]
+          },
+
+          stool: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+          },
+
+          stoolMeasure: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+          },
+
+          stoolColor: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+          },
+
+          stoolNotes: {
+            type: DataTypes.STRING,
+            allowNull: true
+          },
+
+          medicineName: {
+            name: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+              len: [1, 45]
+            }
+          },
+
+          medicineMeasure: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+
+          medicineTime: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+          },
+
+          medicineMeal: {
+            type: DataTypes.STRING,
+            allowNull: true
+          },
+
+          medicineNotes: {
+            type: DataTypes.STRING,
+            allowNull: true
+          },
+
+          walk: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+          run: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+          play: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+          other: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+
+          exerciseTimeMinutes: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+
+          exerciseTimeHours: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+
+          exerciseNotes: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          },
+
+          weight: {
+            type: DataTypes.INTEGER,
+            allowNull: true,
+            validate: {
+              len: [1, 45]
+            }
+          }
+        }
+      }(
+        (tracking.associate = models => {
+          tracking.belongsTo(models.pet, {
+            foreignKey: { allowNull: false }
+          });
+
+          return tracking;
+        })
+      )
+    }
+  });
+};
