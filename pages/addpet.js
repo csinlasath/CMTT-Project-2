1e16bb4a9eb1c232b26dd6694da595d70cf4c13d
Christopher Sinlasath <csinlasath@live.com>
2019-03-21 01:54:38 -0500

    You can now upload a pet profile picture

--- pages/addpet.js
+++ pages/addpet.js
@@ -27,8 +27,17 @@ const AddPetForm = (props) => (
                 <input type="text" className="form-control" id="add-pet-gender" placeholder="Male"></input>
             </div>
             <div className="form-group">
-                <label htmlFor="add-pet-pic" className="text-success">Upload a Picture</label>
-                <input type="file" className="form-control-file" id="add-pet-picture-btn"></input>
+                <div className="row">
+                    <div className="col-md-4">
+                        <label htmlFor="add-pet-pic" className="text-success">Upload a Picture</label>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-12">
+                        <input type="file" className="form-control-file" id="add-pet-picture-btn"></input>
+                    </div>
+                </div>
+                <progress value="0" max="100" role="progressbar" id="add-pet-progress"></progress>
             </div>
             <button id="pet-form-submit" type="submit" className="btn btn-success add-pet-submit">Submit</button>
         </form>
@@ -36,6 +45,14 @@ const AddPetForm = (props) => (
                 #add-pet-form {
                     margin-top: 25px;
                 }
+                progress {
+                    --webkit-appearance: none;
+                    appearance: none;
+                    color: green;
+                    background-color: green;
+                    margin-top: 10px;
+                }
+
                 ::placeholder {
                     opacity: 0.3;
                 }
@@ -46,6 +63,10 @@ const AddPetForm = (props) => (
 
                 #pet-form-title{
                     text-align: center;
+                }
+
+                #progress-bar-wrapper {
+                    margin-top: 10px;
                 }
 
 
